ca5898bdc8d6575bb6e3cec02f3e454ba94be692
BSerge <serge.boskin@icloud.com>
2022-09-27 14:14:22 +0300

    add_Objective_25

---
+++ Objective_25/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите число А: ");
+int numA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число B: ");
+int numB = int.Parse(Console.ReadLine()!);
+
+int total = 1;
+
+Console.WriteLine($"{numA}, {numB} -> {calc(total)}");
+
+int calc (int step)
+    {
+        for (int i = 1; i <= numB; i++)
+        {
+            total = numA * total;
+        }
+    
+return total;
+    }
