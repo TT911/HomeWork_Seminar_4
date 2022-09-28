3cf63ebe69b5797499308c3dfe84d6b0adb8aea9
BSerge <serge.boskin@icloud.com>
2022-09-28 13:46:17 +0300

    add Objective_29

---
+++ Objective_29/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
+
+int[] array = GetArray(8);
+Console.WriteLine($"{String.Join(", ", array)} -> [{String.Join(", ", array)}]");
+
+int[] GetArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(256);
+    }
+    return result;
+}
